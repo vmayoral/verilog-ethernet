3700157745fbcf4559d4e39dd12a5037825dccc7
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2023-03-08 14:12:19 +0100

    Address critical warning and default to IP used elsewhere
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

--- example/KR260/fpga/rtl/fpga_core.v
+++ example/KR260/fpga/rtl/fpga_core.v
@@ -243,8 +243,8 @@ wire tx_fifo_udp_payload_axis_tuser;
 
 // Configuration
 wire [47:0] local_mac   = 48'h02_00_00_00_00_00;
-wire [31:0] local_ip    = {8'd192, 8'd168, 8'd2,   8'd128};
-wire [31:0] gateway_ip  = {8'd192, 8'd168, 8'd2,   8'd1};
+wire [31:0] local_ip    = {8'd192, 8'd168, 8'd1,   8'd128};
+wire [31:0] gateway_ip  = {8'd192, 8'd168, 8'd1,   8'd1};
 wire [31:0] subnet_mask = {8'd255, 8'd255, 8'd255, 8'd0};
 
 // IP ports not used
